a42c73a35ba4bd70d4d8e5d5a3887d23f0a6129d
xueli.xue <xueli.xue@dianping.com>
2016-08-23 14:52:53 +0800

    coding

--- xxl-app/src/main/java/com/xxl/app/ui/ssh/SshPanel.java
+++ xxl-app/src/main/java/com/xxl/app/ui/ssh/SshPanel.java
@@ -1,17 +1,77 @@
 package com.xxl.app.ui.ssh;
 
+import com.xxl.app.util.TiminalUtil;
+
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by xuxueli on 16/8/18.
  */
-public class SshPanel extends Panel {
+public class SshPanel extends Panel implements ActionListener {
 
     private static SshPanel instance;
     public static SshPanel getInstance(){
-        instance = new SshPanel();
-        instance.setBackground(Color.BLACK);
+        if (instance == null) {
+            // 主界面
+            instance = new SshPanel();
+            instance.setBackground(Color.BLACK);
 
+            // go
+            goBtn = new JButton("GO");
+            goBtn.setBackground(Color.BLACK);
+            goBtn.addActionListener(instance);
+            goBtn.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    // super.keyPressed(e);
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        goBtn.doClick();
+                    }
+                }
+            });
+
+            // exit
+            exitBtn = new JButton("EXIT");
+            exitBtn.setBackground(Color.GRAY);
+            exitBtn.addActionListener(instance);
+            exitBtn.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    // super.keyPressed(e);
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        exitBtn.doClick();
+                    }
+                }
+            });
+
+            // 布局
+            instance.setLayout(new FlowLayout());
+            instance.add(goBtn);
+            instance.add(exitBtn);
+
+        }
         return instance;
+    }
+
+    private static JButton goBtn;
+    private static JButton exitBtn;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == goBtn) {
+            Toolkit.getDefaultToolkit().beep();
+            try {
+                TiminalUtil.sshLogin();
+            } catch (Exception e1) {
+                JOptionPane.showMessageDialog(instance, "调用SSH失败:"+e1.getMessage(), null,JOptionPane.ERROR_MESSAGE);
+            }
+        } else if (e.getSource() == exitBtn) {
+            System.exit(0);
+        }
     }
 }
--- xxl-app/src/main/java/com/xxl/app/util/BrowseUtil.java
+++ xxl-app/src/main/java/com/xxl/app/util/BrowseUtil.java
@@ -10,41 +10,46 @@ import java.util.concurrent.TimeUnit;
 public class BrowseUtil {
 
     public static void browse(String url) throws Exception {
-        //String osTitle = System.getProperty("os.title", "");
-        String osName = System.getProperty("os.name", "");
-        if (osName.startsWith("Mac OS")) {
-            Class fileMgr = Class.forName("com.apple.eio.FileManager");
-            Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[]{String.class});
-            openURL.invoke(null, new Object[]{url});
-        } else if (osName.startsWith("Windows")) {
-            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-        } else {
-            // Unix or Linux
-            String[] browsers = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
-            String browser = null;
-            for (int count = 0; count < browsers.length && browser == null; count++) {
-                // 进程创建成功, ==0是表示正常结束
-                if (Runtime.getRuntime().exec(new String[]{"which", browsers[count]}).waitFor() == 0){
-                    browser = browsers[count];
-                }
+
+        OSUtil.OSEnum os = OSUtil.os();
+        switch (os) {
+            case Windows:{
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+                break;
             }
-            if (browser == null) {
-                throw new Exception("Could not find web browser");
-            } else {
-                Runtime.getRuntime().exec(new String[]{browser, url});
+            case Mac:{
+                Class fileMgr = Class.forName("com.apple.eio.FileManager");
+                Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[]{String.class});
+                openURL.invoke(null, new Object[]{url});
+                break;
+            }
+            case LinuxOrUnix:{
+                String[] browsers = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
+                String browser = null;
+                for (int count = 0; count < browsers.length && browser == null; count++) {
+                    // 进程创建成功, ==0是表示正常结束
+                    if (Runtime.getRuntime().exec(new String[]{"which", browsers[count]}).waitFor() == 0){
+                        browser = browsers[count];
+                    }
+                }
+                if (browser == null) {
+                    throw new Exception("Could not find web browser");
+                } else {
+                    Runtime.getRuntime().exec(new String[]{browser, url});
+                }
+                break;
             }
         }
     }
 
-    //主方法 测试类
     public static void main(String[] args) {
-        String url = "http://iteye.blog.163.com/";
         try {
-            BrowseUtil.browse(url);
-
+            BrowseUtil.browse("https://github.com/xuxueli/");
+            BrowseUtil.browse("http://my.oschina.net/xuxueli");
             TimeUnit.SECONDS.sleep(5);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
