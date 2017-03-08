d9bbeb18552a2c2ab13f4bc9276efb792c6da6d2
xueli.xue <xueli.xue@dianping.com>
2017-03-08 14:07:52 +0800

    更新RM

--- demo/test-demo/src/main/java/com/xxl/controller/IndexController.java
+++ demo/test-demo/src/main/java/com/xxl/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.xxl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 /**
  * ehcache 进程缓存, 虽然目前ehcache提供集群方案，但是分布式缓存还是使用memcached/redis比较好;
@@ -23,5 +29,49 @@ public class IndexController {
 	private String index() {
 		return "300";
 	}
-	
+
+	// ----------- redirect 方式（URL）
+	@RequestMapping("/redirect")
+	private String redirect(String word, RedirectAttributes redirectAttributes) {
+		// URL拼参数（动态传参）
+		redirectAttributes.addAttribute("word", "redirect:" + word);
+		// URL不品参数（结合@ModelAttribute）
+		//redirectAttributes.addFlashAttribute("word2", "redirect:" + word);
+		return "redirect:/newUrl";
+	}
+
+	@RequestMapping(value = "/newUrl")
+	@ResponseBody
+	private String newUrl(String word) {
+		return word;
+	}
+
+	// ----------- redirect 方式（addFlashAttribute / Session）(POST-Redirect-GET)
+	@RequestMapping(value = "/redirect1", method = RequestMethod.POST)
+	private String redirect1(String word, RedirectAttributes redirectAttributes) {
+		// URL不品参数（结合@ModelAttribute）
+		redirectAttributes.addFlashAttribute("word", "redirect1:" + word);
+		return "redirect:/newUrl1";
+	}
+
+	@RequestMapping(value = "/newUrl1", method = RequestMethod.GET)
+	@ResponseBody
+	private String newUrl1(String word) {
+		return word;
+	}
+
+	// ----------- forward 方式
+	@RequestMapping("/forward")
+	private String forward(String word, Model model) {
+		model.addAttribute("tim", new Date());
+		return "forward:/forwardUrl";
+	}
+
+	@RequestMapping("/forwardUrl")
+	@ResponseBody
+	private String forwardUrl(String word, HttpServletRequest request, Date tim) {
+		Date tim2 = (Date) request.getAttribute("tim");
+		return ">>>" + word + ":" + tim2.getTime();
+	}
+
 }
