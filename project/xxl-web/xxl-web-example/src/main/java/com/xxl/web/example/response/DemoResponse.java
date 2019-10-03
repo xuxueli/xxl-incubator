9fc10cf6b096df1f3bc504c6598d8034a5c8db70
xuxueli <931591021@qq.com>
2019-10-03 22:06:36 +0800

    update rm

---
+++ project/xxl-web/xxl-web-example/src/main/java/com/xxl/web/example/response/DemoResponse.java
@@ -0,0 +1,14 @@
+package com.xxl.web.example.response;
+
+public class DemoResponse {
+
+	private int sum;
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+}
