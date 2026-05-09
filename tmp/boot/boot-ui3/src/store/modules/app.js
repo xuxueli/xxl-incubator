1af46066d2e04e1259d4938431766e5600521c86
xuxueli <931591021@qq.com>
2026-05-10 01:35:44 +0800

    update doc

--- tmp/boot/boot-ui3/src/store/modules/app.js
+++ tmp/boot/boot-ui3/src/store/modules/app.js
@@ -5,49 +5,72 @@
 import Cookies from 'js-cookie'
 
 const useAppStore = defineStore(
-  'app',
-  {
-    state: () => ({
-      // 侧边栏状态
-      sidebar: {
-        opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
-        withoutAnimation: false,
-        hide: false
-      },
-      // 设备状态
-      device: 'desktop',
-      // 字体大小
-      size: Cookies.get('size') || 'default'
-    }),
-    actions: {
-      toggleSideBar(withoutAnimation) {
-        if (this.sidebar.hide) {
-          return false
+    'app',
+    {
+        state: () => ({
+            // 侧边栏状态
+            sidebar: {
+                opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,    // 0: 关闭，1: 开启
+                withoutAnimation: false,                                                          // 侧边栏切换动画
+                hide: false                                                                       // 侧边栏隐藏
+            },
+            // 设备状态
+            device: 'desktop',
+            // 字体大小
+            size: Cookies.get('size') || 'default'
+        }),
+        actions: {
+            /**
+             * 切换侧边栏状态
+             * @param withoutAnimation   切换动画
+             */
+            toggleSideBar(withoutAnimation) {
+                if (this.sidebar.hide) {
+                    return false
+                }
+                // 切换侧边栏状态
+                this.sidebar.opened = !this.sidebar.opened
+                // 设置是否无动画
+                this.sidebar.withoutAnimation = withoutAnimation
+                // 设置侧边栏状态
+                if (this.sidebar.opened) {
+                    Cookies.set('sidebarStatus', 1)
+                } else {
+                    Cookies.set('sidebarStatus', 0)
+                }
+            },
+            /**
+             * 关闭侧边栏
+             * @param param0.withoutAnimation  设置是否无动画
+             */
+            closeSideBar({withoutAnimation}) {
+                Cookies.set('sidebarStatus', 0)
+                this.sidebar.opened = false
+                this.sidebar.withoutAnimation = withoutAnimation
+            },
+            /**
+             * 设置设备状态
+             * @param device  设备状态
+             */
+            toggleDevice(device) {
+                this.device = device
+            },
+            /**
+             * 设置字体大小
+             * @param size  字体大小
+             */
+            setSize(size) {
+                this.size = size
+                Cookies.set('size', size)
+            },
+            /**
+             * 侧边栏隐藏
+             * @param status  侧边栏隐藏状态
+             */
+            toggleSideBarHide(status) {
+                this.sidebar.hide = status
+            }
         }
-        this.sidebar.opened = !this.sidebar.opened
-        this.sidebar.withoutAnimation = withoutAnimation
-        if (this.sidebar.opened) {
-          Cookies.set('sidebarStatus', 1)
-        } else {
-          Cookies.set('sidebarStatus', 0)
-        }
-      },
-      closeSideBar({ withoutAnimation }) {
-        Cookies.set('sidebarStatus', 0)
-        this.sidebar.opened = false
-        this.sidebar.withoutAnimation = withoutAnimation
-      },
-      toggleDevice(device) {
-        this.device = device
-      },
-      setSize(size) {
-        this.size = size
-        Cookies.set('size', size)
-      },
-      toggleSideBarHide(status) {
-        this.sidebar.hide = status
-      }
-    }
-  })
+    })
 
 export default useAppStore
